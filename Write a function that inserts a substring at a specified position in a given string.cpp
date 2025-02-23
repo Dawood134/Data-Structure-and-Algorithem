750c82ec355d573402baf5daae0ad426c117c8ee
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2025-02-23 22:12:42 +0500

    Add files via upload

---
+++ Write a function that inserts a substring at a specified position in a given string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	string name = "Hello World";
+	string add = "beautiful";
+	string result;
+		int index =6;
+		for (int i = index; i < add.length(); i++) {
+			result[i] = add[i];
+	}
+		cout << result;
+}
