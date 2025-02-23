5ef1ee1108a03ee6a0d695475165c492b95a9e00
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2025-02-23 22:15:02 +0500

    Update Write a function that inserts a substring at a specified position in a given string.cpp

--- Write a function that inserts a substring at a specified position in a given string.cpp
+++ Write a function that inserts a substring at a specified position in a given string.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 using namespace std;
-
-int main() {
-	string name = "Hello World";
-	string add = "beautiful";
-	string result;
-		int index =6;
-		for (int i = index; i < add.length(); i++) {
-			result[i] = add[i];
+void insertNewString(string original, string substring,int index) {
+	string result = "";
+	for (int i = 0; i < index; i++) {
+		result += original[i];
 	}
-		cout << result;
+	for (int i = 0; i < substring.length(); i++) {
+		result += substring[i];
+	}
+	for (int i = index; i < original.length(); i++) {
+		result += original[i];
+	}
+	cout << result;
+}
+int main() {
+	string original = "Hello Dawood ";
+	string substring = "Handsome ";
+	int index =0;
+	cout << "Enter index in which you want to insert substring :" << endl;
+	cin >> index;
+	insertNewString(original, substring, index);
 }
